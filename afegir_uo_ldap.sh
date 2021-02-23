4c8946886dfdf43bc346adf8413025707575db4d
mperalsapa <61291001+mperalsapa@users.noreply.github.com>
2021-02-23 07:26:58 +0100

    Create afegir_uo_ldap.sh

---
+++ afegir_uo_ldap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+read -p "Nom del fitxer resultant [fitxer.ldif]:" nomfitxer
+echo "$2"
+echo "---- Fitxer d'usuaris ----"
+echo "$1"
+echo "--------------------------"
+touch "$nomfitxer"
+echo "">"$nomfitxer"
+
+echo "Recorda: Es generara un fitxer amb el nom $nomfitxer"
+echo "El podrás executar amb sudo ldapadd -x -D cn=admin,dc=domini,dc=com -W -f $nomfitxer"
+
+while true;
+do
+        read -p "Nom de la Unitat Organitzativa :" nomUo
+        read -p "Ruta de la UO [uo=patata,dc=dom,dc=com]:" rutaUo
+        echo "dn: $rutaUo" >> $nomfitxer
+        echo "objectClass: organizationalUnit" >> $nomfitxer
+        echo "ou: $nomUo" >> $nomfitxer
+        echo "" >> $nomfitxer
+        echo "per sortir del programa prem ctrl+c, les dades introduides s'han guardat."
+done
